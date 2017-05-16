09db28193164ed015c7197917d0cf250386c611b
pgmkhp <pgmkhp@protonmail.com>
2017-05-16 10:29:37 +0800

    合并到index

--- js/main.js
+++ js/main.js
@@ -6,27 +6,50 @@ var imgdescription; // 图像描述
 var startdate; // 开始日期
 var enddate; // 结束日期
 
-var curcomp = null; //当前选中组件
+var curcomp = null; // 当前选中组件
+var curline = null; // 当前选中线
+
+// 线的样式
+var line1 = {stroke: "green", strokeWidth:2};
+var line2 = {stroke: "green", strokeWidth:2, dashstyle: "5 2"};
+var line3 = {stroke: "red", strokeWidth:2};
+var line4 = {stroke: "green", strokeWidth:6};
+
+// 线、组件的数据结构
+var line1data = new Graph();
+var line2data = new Graph();
+var line3data = new Graph();
+var line4data = new Graph();
 
 // 禁用右键菜单
 document.oncontextmenu = function() {
     return false;
 }
 
-// 组件创建函数，接受一个组件的class，返回一个JQuery对象
+// 组件创建函数，接受一个组件的class
 function createComponent(classname) {
     var comp = $(document.createElement('div'));
     comp.addClass(classname);
     comp.addClass('component');
     comp.html('未命名');
     comp.appendTo($('.board'));
+    // 添加组件id
+    var id = jsPlumbUtil.uuid();
+    comp.attr('id', id);
+    line1data.addVertex(id);
 
-    // comp.draggable({containment:'parent'});
     jsPlumb.draggable(comp, {containment: "parent"});
-    // jsPlumb.makeTarget(comp, {detachable:false});
+    // 双击更改组件名称
     comp.dblclick(function() {
         curcomp = comp;
         $('#modifyModal').modal('toggle'); // 显示模态框
+    });
+    // 右键删除组件
+    comp.bind('mousedown', function(even) {
+        if (even.which == 3) {
+            jsPlumb.remove(comp);
+            line1data.delVertex(id);
+        }
     });
 }
 
@@ -40,6 +63,19 @@ $('#renamebtn').click(function() {
     }
 });
 
+jsPlumb.ready(function() { 
+    jsPlumb.importDefaults({
+        Anchor: "AutoDefault",
+        Endpoint: ["Dot", {radius: 3}],
+        DragOptions: {cursor: "crosshair" },
+        Container: "board",
+        ConnectionOverlays: [
+            ["PlainArrow", {location: 1}],
+            ["Label", {label: "未命名"}]
+        ],
+    });   
+});
+
 // 画板工具栏事件绑定
 $('#component1').click(function() {
     createComponent('component1');
@@ -50,61 +86,85 @@ $('#component2').click(function() {
 $('#component3').click(function() {
     createComponent('component3');
 });
+$('#line1').click(function() {
+    jsPlumb.importDefaults({PaintStyle: line1});
+});
+$('#line2').click(function() {
+    jsPlumb.importDefaults({PaintStyle: line2});
+});
+$('#line3').click(function() {
+    jsPlumb.importDefaults({PaintStyle: line3});
+});
+$('#line4').click(function() {
+    jsPlumb.importDefaults({PaintStyle: line4});
+});
 $('.linebtn').click(function() {
     jsPlumb.toggleDraggable($('.component'));
     jsPlumb.makeSource($('.component'), {detachable:false});
     jsPlumb.makeTarget($('.component'), {detachable:false});
     $(this).addClass('btn-primary');
 });
-$('#line1').click(function() {
-});
+
+// 连线事件
 jsPlumb.bind("connection", function(conn, originalEvent) {
-    // $('.component').draggable("enable");
     jsPlumb.toggleDraggable($('.component'));
-    // jsPlumb.draggable($('.component'));
     jsPlumb.unmakeEverySource();
     jsPlumb.unmakeEveryTarget();
     $('.linebtn').removeClass('btn-primary');
+
+    line1data.addEdge(conn.sourceId, conn.targetId);
 });
+
+// 鼠标右键连线，删除连线
 jsPlumb.bind("mousedown", function(conn, originalEvent) {
     if (originalEvent.which == 3) {
         jsPlumb.detach(conn);
+
+        line1data.delEdge(conn.sourceId, conn.targetId);
     }      
 });
-jsPlumb.ready(function() { 
-    jsPlumb.importDefaults({
-        Anchor: "AutoDefault",
-        Endpoint: ["Dot", {radius: 3}],
-        DragOptions: {cursor: "crosshair" },
-        Container: "board",
-    });
-    var line1 = jsPlumb.getInstance({
-        PaintStyle: {stroke: "green", strokeWidth:3},
-        // PaintStyle: {stroke: "green", strokeWidth:3, dashstyle: "5 2"},
-        Overlays: [
-            ["PlainArrow", {location: 1}],
-            ["Label", {label: "未命名"}]
-        ],
-    });
-});
+
 
 
 
 
 
 // 存放图的数据结构
-function Graph(v) {
-    this.vertices = v;
+function Graph() {
+    this.vertices = 0;
     this.edges = 0;
     this.adj = [];
-    for(var i =0; i<this.vertices; i++) {
-        this.adj[i] = [];
-        // this.adj[i].push('');
-    }
+    
+    this.addVertex = addVertex;
+    this.delVertex = delVertex;
     this.addEdge = addEdge;
-    // this.toString = toString;
+    this.delEdge = delEdge;
+    this.getData = getData;
 }
-function addEdge(id1, id2) {
-    this.adj[id1].push(id2);
+function addVertex(id) {
+    this.vertices++;
+    this.adj[id] = [];
+}
+function delVertex(id) {
+    this.vertices--;
+    this.edges -= this.adj[id].length;
+    delete this.adj[id];
+}
+function addEdge(sid, tid) {
+    this.adj[sid].push(tid);
     this.edges++;
+}
+function delEdge(sid, tid) {
+    this.adj[sid].splice($.inArray(tid, this.adj[sid]),1);
+    this.edges--;
+}
+function getData() {
+    var data = [];
+    for (var sid in this.adj) {
+        for (var i=0; i<this.adj[sid].length; i++) {
+            var tid = this.adj[sid][i];
+            data.push([sid, tid]);
+        }
+    }
+    return data;
 }
